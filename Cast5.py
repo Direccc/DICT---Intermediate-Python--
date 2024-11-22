01e0c8ffb85d546a0171cf51e61ddf246579e29c
Direccc <112394828+Direccc@users.noreply.github.com>
2024-11-22 22:41:04 +0800

    Add files via upload

--- Cast5.py
+++ Cast5.py
@@ -1,25 +1,50 @@
 #Intermediate Coding Exercise 1.5
-class Customer():
-    greeting = "Welcome to Coffee Palace!"
-    def __init__(self,name,beverage,food,total):
-        self.name = name
-        self.beverage = beverage
-        self.food = food
-        self.total = total
-    
+class Customers:
+    greeting = "Welcome to the Coffee Palace!"
+    def __init__(self, Name, Beverage, Food, Total):
+        self.Name = Name
+        self.Beverage = Beverage
+        self.Food = Food
+        self.Total = Total
+
     def __str__(self):
-        return f"Name: {self.name}, Beverage: {self.beverage}, Food: {self.food}, Total: {self.total}"
+        return f"Customer Name: {self.Name}\n Beverage: {self.Beverage}\n Food: {self.Food}\n Total: {self.Total}\n"
+
+c_1 = Customers("Nate", "Espresso", "Pastrami on rye", 220)
+c_2 = Customers("Elaine", "Strawberry Frappuccino", "Tuna wrap", 270)
+c_3 = Customers("Samirah", "Iced caffe latte", "Cinnamon roll", 225)
+c_4 = Customers("Jerry", "Caramel macchiato", "Glazed doughnut", 230)
+c_5 = Customers("Paz", "Iced tea", "Blueberry pancakes", 315)
 
 
-print(Customer.greeting)
-c_1 = Customer("Nate","Espresso","Pastrami on Rye",220)
-c_2 = Customer("Elaine","Strawberry frappuccino","Tuna wrap",270)
-c_3 = Customer("Samirah","Iced caffe latte","Cinnamon roll",225)
-c_4 = Customer("Jerry","Caramel macchiato","Glazed doughnut",230)
-c_5 = Customer("Paz","Iced tea","Blueberry pancakes",315)
-
+print(Customers.greeting)
 print(c_2)
 print(c_4)
-
-print("")
 #Ending of Intermediate Coding Exercise 1.5
+
+#Intermediate Coding Exercise 1.9
+class ClubMembers():
+    def __init__(self, Name, Birthday, Age , Favorite_food, Goal):
+        self.Name = Name
+        self.Birthday = Birthday
+        self.Age = Age
+        self.Favorite_food = Favorite_food
+        self.Goal = Goal
+
+    def __str__(self):
+        return f"Name: {self.Name}\n Birthday: {self.Birthday}\n Age: {self.Age}\n Favorite Food: {self.Favorite_food}\n Goal: {self.Goal}\n"
+    
+class ClubOfficers(ClubMembers):
+    def __init__(self, Name, Birthday, Age, Favorite_food, Goal,Position):
+        self.Position = Position
+        super().__init__(Name, Birthday, Age, Favorite_food, Goal)
+
+    def __str__(self):
+        return f"{super().__str__()}Position: {self.Position}"
+    
+m_1 = ClubMembers("Tom", "January 16", "14", "Ice Cream", "To be happy")
+o_4 = ClubOfficers("Vera", "June 22", "16", "Beef stroganoff", "To be the world's greatest chef", "Treasurer")
+
+print(m_1)
+print(o_4)
+#End of Intermediate Coding Exercise 1.9
--- Cast6.py
+++ Cast6.py
@@ -1,5 +1,41 @@
+#Intermediate Coding Exercise 2.4
+Flavor = ["Vanilla", "Chocolate", "Strawberry", "Cookies N' Cream", "BubbleGum"]
+Toppings = ["Almonds", "Banana Slices", "Chocolate Syrup", "Caramel Syrup", "White Chocolate Chips"]
 
+ice_cream = dict(zip(Flavor, Toppings))
+print (ice_cream)
 
-groceries = {"chicken": 8, "apples": 6, "cucumbers":3, "milk": 2, "oranges":4}
-remove = groceries.pop("oranges")
+ice_cream["Chocolate"] = "BlueBerries"
+ice_cream.update ({"Matcha" : "Pistachios", "Ube" : "Mango Slices"})
+print(ice_cream)
+#Ending Intermediate Coding Exercise 2.4
+
+#Intermediate Coding Exercise 2.8
+groceries = {"Chicken" : 8, "Apples" : 6, "Cucumbers" : 3, "Milk" : 2, "Oranges" : 4}
+groceries.pop("Oranges")
+
 print(groceries)
+
+speakers = {"Sir Rafael" : 54, "Ms. Joan" : 33, "Ms. Dana" : 67}
+names = speakers.keys()
+print(names)
+
+Results = {
+    "Carl": "passed",
+    "Quentin": "failed",
+    "John Y.": "passed",
+    "Peter": "failed",
+    "Max T.": "passed",
+    "Joseph": "passed",
+    "Jone": "failed",
+    "Jorge": "failed",
+    "George": "passed",
+    "Ben": "passed",
+    "Jerome": "passed",
+    "Rick": "failed",
+    "Max G.": "failed",
+    "John P.": "failed",
+    "Vince": "passed"
+}
+
+print(Results.get("Jorge"))
