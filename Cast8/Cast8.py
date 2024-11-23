61d9af28724b03df7f7f132a4d9a4a79374504ad
Direccc <112394828+Direccc@users.noreply.github.com>
2024-11-24 04:03:56 +0800

    Add files via upload

---
+++ Cast8/Cast8.py
@@ -0,0 +1,18 @@
+from functionfile import Age
+from functionfile import Goal
+from time import sleep
+
+#Intermediate Coding Exercise 4.4
+#module = open("functionfile.py", "x")
+
+Goal("Data Analyst")
+Age("20")
+
+#Ending Intermediate Coding Exercise 4.4
+
+#help("modules")
+#help("time")
+
+x = "Somebody that I used to know"
+sleep(3)
+print(x)
---
+++ Cast8/functionfile.py
@@ -0,0 +1,15 @@
+#Intermediate Coding Exercise 4.4
+def Name(name):
+    print("My name is " + name)
+def Age(age):
+    print("I am " + age + "years old!")
+def Goal(goal):
+    print("My goal for in the near future is to be a " + goal)
+def Status(status):
+    print("I am currently " + status)
+def Number(number):
+    print("My number is " + number)
+#Ending Intermediate Coding Exercise 4.4
+
+
+("modules")
